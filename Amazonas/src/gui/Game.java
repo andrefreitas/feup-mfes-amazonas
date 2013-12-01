354157fa6888e6256554521509e90fce986adec3
Rui Gonçalves <rui.nf1@gmail.com>
2013-12-01 19:58:36 +0000

    Continued to implement the GUI: 	Implemented the mouse listener 	Started to Implemented a method on gui/Game class to draw the pieces on the board 	Corrected layout bugs

--- Amazonas/src/gui/Game.java
+++ Amazonas/src/gui/Game.java
@@ -1,15 +1,22 @@
 package gui;
 
+import java.awt.Graphics;
+
 /**
  * @author Ana Gomes, Rui Gonçalves, André Freitas
  */
 public class Game {
 
-    public static Board gameBoardWindow;
+    public static BoardFrame gameBoardWindow;
     public static Startup gameStartupWindow;
     
     public static void main(String[] args) {
         gameStartupWindow = new Startup(null);
+    }
+    
+    public static void drawPieces(Graphics g){
+        //Aqui é aonde vao ser desenhadas as peças
+        g.drawImage(BoardPanel.blackPiece, 20, 0, gameBoardWindow);
     }
     
 }
